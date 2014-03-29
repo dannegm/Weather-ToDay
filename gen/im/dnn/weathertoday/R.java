402c8e6cca99f822303ea589eb99ad162d4b1e8e
dannegm <danneg.m@gmail.com>
2014-03-29 02:34:54 -0600

    eclipse autogen

--- gen/im/dnn/weathertoday/R.java
+++ gen/im/dnn/weathertoday/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int bgActioBar=0x7f040003;
         public static final int gray_aplha=0x7f040001;
         public static final int purple=0x7f040002;
+        public static final int purple2=0x7f040004;
         public static final int white=0x7f040000;
     }
     public static final class dimen {
@@ -106,16 +107,16 @@ public final class R {
         public static final int tstorms=0x7f020048;
     }
     public static final class id {
-        public static final int BG_Weather=0x7f090001;
-        public static final int RelLayout=0x7f090000;
+        public static final int BG_Weather=0x7f090000;
         public static final int action_refresh=0x7f090007;
         public static final int action_search=0x7f090008;
         public static final int action_settings=0x7f090009;
-        public static final int cityname=0x7f090002;
-        public static final int datedisplay=0x7f090006;
-        public static final int degress_down=0x7f090005;
-        public static final int degress_up=0x7f090004;
-        public static final int iconWeather=0x7f090003;
+        public static final int dateDisplay=0x7f090004;
+        public static final int degress_up=0x7f090002;
+        public static final int iconWeather=0x7f090001;
+        public static final int imageView1=0x7f090005;
+        public static final int textView1=0x7f090006;
+        public static final int weatherDisplay=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
